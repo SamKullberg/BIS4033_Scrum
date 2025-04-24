84258f95214499dcc18fb0667752db8c91c5d261
Jacob Silberfarb <thegreatgeeker@gmail.com>
2025-04-24 10:00:24 -0500

    Update app_config.php

--- app_config.php
+++ app_config.php
@@ -1,11 +1,17 @@
 <?php 
-//application folder name on your web root or htdocs folder
-define('APP_ROOT',$_SERVER['DOCUMENT_ROOT']);
-//DEBUG echo (APP_ROOT)."      <br>";
+// Application folder name on your web root or htdocs folder
+define('APP_ROOT', $_SERVER['DOCUMENT_ROOT']);
+
+// Define your application folder name
 define('APP_FOLDER_NAME', '/myCrud');
-define('WEB_ROOT','http://'.$_SERVER['SERVER_NAME']);
-//DEBUG echo(WEB_ROOT);
-define('DSN1', 'mysql:host=localhost;dbname=phpcrud');
-define('USER1','root');
-define('PASSWD1','');
+
+// Define your web root (domain)
+define('WEB_ROOT', 'http://' . $_SERVER['SERVER_NAME']);
+
+// Update the DSN to point to the BIS4033Scrum database
+define('DSN1', 'mysql:host=localhost;dbname=BIS4033Scrum');
+
+// MySQL username and password
+define('USER1', 'root');       // Or replace with your actual MySQL user, e.g., 'kermit'
+define('PASSWD1', '');         // Update if your MySQL user has a password
 ?>
