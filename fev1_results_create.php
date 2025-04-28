cbc5578ce4d111d8e28a279d5fb9757b6ee6222b
gms1200 <98053034+gms1200@users.noreply.github.com>
2025-04-28 09:11:54 -0500

    Update fev1_results_create.php

---
+++ fev1_results_create.php
@@ -0,0 +1,51 @@
+<?php
+@include_once ('../../app_config.php');
+@include_once (APP_ROOT.APP_FOLDER_NAME . '/scripts/functions.php');
+$pdo = pdo_connect_mysql();
+$msg = '';
+// Check if POST data is not empty
+if (!empty($_POST)) {
+    // Post data not empty insert a new record
+    // Set-up the variables that are going to be inserted, //we must check if the POST variables exist if not we //can default them to blank
+    $fev1_id = isset($_POST['fev1_id']) && !empty($_POST['fev1_id']) && $_POST['fev1_id'] != 'auto' ? $_POST['fev1_id'] : NULL;
+    /*Check if POST variable //"fev1_value" exists, if not default //the value to blank, basically the same for //all variables*/
+    $fev1_value = isset($_POST['fev1_value']) ? $_POST['fev1_value'] : '';
+    $visit_id = isset($_POST['visit_id']) ? $_POST['visit_id'] : '';
+    // Insert new record into the fev1_results table
+    $stmt = $pdo->prepare('INSERT INTO fev1_results VALUES (?, ?, ? )');
+    $stmt->execute([$fev1_id, $fev1_value, $visit_id]);
+    // Output message
+    $msg = 'Created Successfully!';
+}
+?>
+<?=template_header('Create')?>
+
+<div class="content update">
+	<h2>Create Results</h2>
+    <form action="fev1_results_create.php" method="post">
+        <label for="fev1_id">ID</label>
+        <label for="fev1_value">Value</label>
+        <input type="text" name="fev1_id" placeholder="26" value="auto" fev1_id="fev1_id">
+        <input type="number" name="fev1_value" placeholder="9867356" fev1_id="fev1_value">
+        <?php
+        $stmt = $pdo->query("SELECT fev1_id, name FROM fev1_results");
+        $fev1_results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      
+        ?>
+        <label for="visit_id">Customer ID</label>
+        
+         <select name="visit_id" fev1_id="visit_id">
+            <?php foreach($visits as $visit) : ?>
+            <option value="<?php echo $visit['visit_id']; ?>"><?php echo $visit['visit_id'] . ' - ' . $visit['name']; ?></option>
+            <?php endforeach; ?>
+        </select>
+        
+
+        <input type="submit" value="Create">
+    </form>
+    <?php if ($msg): ?>
+    <p><?=$msg?></p>
+    <?php endif; ?>
+</div>
+
+<?=template_footer()?>
