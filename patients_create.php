f5bbc34306da243088c706d4776f6bec11cf9f22
SamKullberg <122641437+SamKullberg@users.noreply.github.com>
2025-04-29 14:28:02 -0500

    Update patients_create.php

--- patients_create.php
+++ patients_create.php
@@ -7,17 +7,17 @@ $msg = '';
 if (!empty($_POST)) {
     // Post data not empty insert a new record
     // Set-up the variables that are going to be inserted, //we must check if the POST variables exist if not we //can default them to blank
-    $patientId = isset($_POST['patientId']) && !empty($_POST['patientId']) && $_POST['patientId'] != 'auto' ? $_POST['patientId'] : NULL;
+    $patientId = isset($_POST['patient_id']) && !empty($_POST['patient_id']) && $_POST['patient_id'] != 'auto' ? $_POST['patient_id'] : NULL;
     // Check if POST variable "name" exists, if not default //the value to blank, basically the same for all //variables
-    $patientFName = isset($_POST['patientFName']) ? $_POST['patientFName'] : '';
-    $patientLName = isset($_POST['patientLName']) ? $_POST['patientLName'] : '';
-    $patientGender = isset($_POST['patientGender']) ? $_POST['patientGender'] : '';
-    $patientBDay = isset($_POST['patientBDay']) ? $_POST['patientBDay'] : '';
-    $patientGenetic = isset($_POST['patientGenetic']) ? $_POST['patientGenetic'] : '';
-    $patientDiabetes = isset($_POST['patientDiabetes']) ? $_POST['patientDiabetes'] : '';
-    $patientCond = isset($_POST['patientCond']) ? $_POST['patientCond'] : '';
+    $patientFName = isset($_POST['first_name']) ? $_POST['first_name'] : '';
+    $patientLName = isset($_POST['last_name']) ? $_POST['last_name'] : '';
+    $patientGender = isset($_POST['gender']) ? $_POST['gender'] : '';
+    $patientBDay = isset($_POST['birthdate']) ? $_POST['birthdate'] : '';
+    $patientGenetic = isset($_POST['genetics']) ? $_POST['genetics'] : '';
+    $patientDiabetes = isset($_POST['diabetes']) ? $_POST['diabetes'] : '';
+    $patientCond = isset($_POST['other_conditions']) ? $_POST['other_conditions'] : '';
    
-    $stmt = $pdo->prepare('INSERT INTO patients (patientId, patientFName, patientLName, patientGender, patientBDay, patientGenetic, patientDiabetes, patientCond) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+    $stmt = $pdo->prepare('INSERT INTO patients (patient_id, first_name, last_name, gender, birthdate, genetics, diabetes, other_conditions) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
     $stmt->execute([$patientId, $patientFName, $patientLName, $patientGender, $patientBDay, $patientGenetic, $patientDiabetes, $patientCond]);
     // Output message
     $msg = 'Created Successfully!';
@@ -28,41 +28,29 @@ if (!empty($_POST)) {
 <div class="content update">
 	<h2>Create Patient</h2>
     <form action="patients_create.php" method="post">
-<div>
-        <label for="patientId">Patient ID</label>
-</div>
-<div>
-        <label for="patientFName">First Name</label>
-        <input type="text" name="patientFName" placeholder="John" id="name">
-</div>
-<div>
-        <label for="patientLName">Last Name</label>
-        <input type="text" name="patientLName" placeholder="Doe" id="name">
-</div>
-<div>
-        <label for="patientGender">Gender</label>
-        <input type="text" name="patientGender" id="patientGender">
-</div>
-<div>
-        <label for="patientBDay">Birthdate</label>
-        <input type="date" name="patientBDay" id="patientBDay">
-</div>
-<div>
-        <label for="patientGenetic">Genetics</label>
-        <input type="text" name="patientGenetic" id="patientGenetic">
-</div>
-<div>
-    <label for="patientDiabetes">Diabetes</label>
-    <select name="patientDiabetes" id="patientDiabetes">
-        <option value="Yes">Yes</option>
-        <option value="No">No</option>
-    </select>
-</div>
-<div>
-        <label for="patientCond">Other Conditions</label>
-        <input type="text" name="patientCond" id="patientCond">
-</div>
 
+        <label for="patient_id">Patient ID</label>
+        <input type="text" name="patient_id" placeholder="26" value="auto" id="patient_id"><br>
+        <label for="first_name">First Name</label>
+        <input type="text" name="first_name" placeholder="John" id="first_name"><br>
+
+        <label for="last_name">Last Name</label>
+        <input type="text" name="last_name" placeholder="Doe" id="last_name"><br>
+        <label for="gender">Gender</label>
+        <input type="text" name="gender" id="gender"><br>
+
+        <label for="birthdate">Birthdate</label>
+        <input type="date" name="birthdate" id="birthdate"><br>
+        <label for="genetics">Genetics</label>
+        <input type="text" name="genetics" id="genetics"><br>
+
+        <label for="other_conditions">Other Conditions</label>
+        <input type="text" name="other_conditions" id="other_conditions"><br>
+        <label for="diabetes">Diabetes</label>
+        <select name="diabetes" id="diabetes">
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+        </select><br>
         <input type="submit" value="Create">
     </form>
     <?php if ($msg): ?>
