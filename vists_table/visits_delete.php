be0a19e687c7a1ae3aa84d62e124e8a611386087
SamKullberg <122641437+SamKullberg@users.noreply.github.com>
2025-04-29 14:19:53 -0500

    Update visits_delete.php

--- vists_table/visits_delete.php
+++ vists_table/visits_delete.php
@@ -1,21 +1,22 @@
 <?php
-// @include_once ('../../app_config.php');
-// @include_once (APP_ROOT.APP_FOLDER_NAME . '/scripts/functions.php');
-// $pdo = pdo_connect_mysql();
+@include_once ('../../app_config.php');
+@include_once (APP_ROOT.APP_FOLDER_NAME . '/scripts/functions.php');
+$pdo = pdo_connect_mysql();
 $msg = '';
 
-if (isset($_GET['id'])) {
+if (isset($_GET['visit_id'])) {
     $stmt = $pdo->prepare('SELECT * FROM visits WHERE visit_id = ?');
-    $stmt->execute([$_GET['id']]);
+    $stmt->execute([$_GET['visit_id']]);
     $visit = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$visit) {
-        exit('Contact doesn\'t exist with that ID!');
+        exit('Visit doesn\'t exist with that ID!');
     }
     if (isset($_GET['confirm'])) {
         if ($_GET['confirm'] == 'yes') {
             $stmt = $pdo->prepare('DELETE FROM visits WHERE visit_id = ?');
-            $stmt->execute([$_GET['id']]);
-            header('Location: visits_read.php'); 
+            $stmt->execute([$_GET['visit_id']]);
+            $msg = 'You have deleted the visit!';
+            header('Location: visits_read.php');
             exit; 
         } else {
             header('Location: visits_read.php');
@@ -26,7 +27,7 @@ if (isset($_GET['id'])) {
     exit('No ID specified!');
 }
 ?>
-<?php //template_header('Delete')?>
+<?php template_header('Delete')?>
 
 <div class="content delete">
 	<h2>Delete Visit #<?=$visit['visit_id']?></h2>
@@ -35,10 +36,10 @@ if (isset($_GET['id'])) {
     <?php else: ?>
 	<p>Are you sure you want to delete visit #<?=$visit['visit_id']?>?</p>
     <div class="yesno">
-        <a href="visits_delete.php?id=<?=$visit['visit_id']?>&confirm=yes">Yes</a>
-        <a href="visits_delete.php?id=<?=$visit['visit_id']?>&confirm=no">No</a>
+    <a href="visits_delete.php?visit_id=<?=$visit['visit_id']?>&confirm=yes">Yes</a>
+    <a href="visits_delete.php?visit_id=<?=$visit['visit_id']?>&confirm=no">No</a>
     </div>
     <?php endif; ?>
 </div>
 
-<? //template_footer()?>
+<? template_footer()?>
