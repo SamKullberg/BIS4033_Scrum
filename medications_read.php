cf474d0428f57818736acd7f4e5bb4d45591bbee
SamKullberg <122641437+SamKullberg@users.noreply.github.com>
2025-04-29 14:26:37 -0500

    Update medications_read.php

--- medications_read.php
+++ medications_read.php
@@ -1,6 +1,6 @@
 <?php
-@include_once('../app_config.php');
-@include_once (APP_ROOT.APP_FOLDER_NAME . '../functions.php');
+@include_once ('../../app_config.php');
+@include_once (APP_ROOT.APP_FOLDER_NAME . '/scripts/functions.php');
 
 // Connect to MySQL database
 $pdo = pdo_connect_mysql();
@@ -24,7 +24,7 @@ $medications = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $total_medications = (int)$pdo->query('SELECT COUNT(*) FROM medications')->fetchColumn();
 ?>
 
-<?=template_header('Read Medications')?>
+<?=template_header('Read')?>
 
 <div class="content read">
     <h2>Medications List</h2>
@@ -34,37 +34,34 @@ $total_medications = (int)$pdo->query('SELECT COUNT(*) FROM medications')->fetch
             <tr>
                 <td>#</td>
                 <td>Medication Name</td>
-                <td>Actions</td>
+                <td>Requires Quantity</td>
+                <td>Requires Date</td>
+                <td></td>
             </tr>
         </thead>
         <tbody>
-            <?php if ($medications): ?>
-                <?php foreach ($medications as $medication): ?>
-                <tr>
-                    <td><?=htmlspecialchars($medication['medication_id'])?></td>
-                    <td><?=htmlspecialchars($medication['name'])?></td>
-                    <td class="actions">
-                        <a href="medications_update.php?id=<?=urlencode($medication['medication_id'])?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
-                        <a href="medications_delete.php?id=<?=urlencode($medication['medication_id'])?>" class="trash" onclick="return confirm('Are you sure you want to delete this medication?')"><i class="fas fa-trash fa-xs"></i></a>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td colspan="3">No medications found.</td>
-                </tr>
-            <?php endif; ?>
+            <?php foreach ($medications as $medication): ?>
+            <tr>
+                <td><?=$medication['medication_id']?></td>
+                <td><?=$medication['name']?></td>
+                <td><?=$medication['requires_quantity']?></td>
+                <td><?=$medication['requires_date']?></td>
+                <td class="actions">
+                    <a href="medications_update.php?medication_id=<?=$medication['medication_id']?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                    <a href="medications_delete.php?medication_id=<?=$medication['medication_id']?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
         </tbody>
     </table>
-
-    <div class="pagination">
-        <?php if ($page > 1): ?>
-            <a href="medications_read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
-        <?php endif; ?>
-        <?php if ($page * $records_per_page < $total_medications): ?>
-            <a href="medications_read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
-        <?php endif; ?>
-    </div>
+	<div class="pagination">
+		<?php if ($page > 1): ?>
+		<a href="medications_read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
+		<?php endif; ?>
+		<?php if ($page*$records_per_page < $total_medications): ?>
+		<a href="medications_read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
+		<?php endif; ?>
+	</div>
 </div>
 
 <?=template_footer()?>
